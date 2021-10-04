00c47cb6c0c6da45d169625d806eb43460f681ac
Arman Habib <armanadd01@gmail.com>
2021-10-04 16:28:45 +0600

    commit

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import aboutImgOne from '../../images/about-img01.webp';
-import aboutImgTwo from '../../images/about-img02.webp';
+import aboutImgOne from '../../images/about-img01.jpg';
+import aboutImgTwo from '../../images/about-img02.jpg';
 
 const About = () => {
     return (
@@ -10,13 +10,13 @@ const About = () => {
                     <img className="img-fluid" src={aboutImgOne} alt="" />
                 </div>
                 <div className="col-md-6">
-                    <h1  className="my-3 py-3 border-bottom border-primary fs-3">Become an  <span className="fs-2 fw-bold text-danger">Instructor</span></h1>
+                    <h1  className="my-3 py-3 fs-3">Become an Instructor</h1>
                     <p>Instructors from around the world teach millions of students on Udemy. We provide the tools and skills to teach what you love.</p>
                 </div>
             </div>
             <div className="row justify-content-center align-items-start mt-5">
                 <div className="col-md-6 col-12">
-                    <h1  className="my-3 my-3 py-3 border-bottom border-primary fs-3">Transform your life through  <span className="fs-2 fw-bold text-danger">Education</span></h1>
+                    <h1  className="my-3 my-3 py-3  fs-3">Transform your life through Education</h1>
                     <p>Learners around the world are launching new careers, advancing in their fields, and enriching their lives.</p>
                 </div>
                 <div className="col-md-6">
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,7 +14,7 @@ const Home = () => {
         <div className="main container-fluid">
             <div className="row">
                 <div className="banner col-md-12 col-12">
-                    <h1 className="text-center text-dark fs-1">Let's Study Together</h1>
+                    <h1 className="text-center text-light fs-1">Let's Study Together</h1>
                 </div>
             </div>
             <div className=" py-5 mt-5 text-center">
